575dad21734d9ae690ab28000053f55050bffce9
ak9748564 <122973008+ak9748564@users.noreply.github.com>
2023-11-12 10:44:01 +0530

    Update activityRoute.js

--- routes/activityRoute.js
+++ routes/activityRoute.js
@@ -4,6 +4,7 @@ const activity = express();
 const multer = require('multer');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Contact=require("./../models/contactModel")
 
 activity.use(bodyParser.urlencoded({extended:true}));
 activity.use(express.static(path.resolve(__dirname,'public')))
@@ -22,7 +23,23 @@ var upload = multer({storage:storage});
 const activityController = require('./../controllers/activityController');
 
 activity.post('/importActivity',upload.single('file'),activityController.importActivity);
-activity.post('/contact',activityController.contact);
+activity.post('/contact',async(req,res) => {
+    try {
+        const {name,email,phone,message,activity}=req.body
+        const saveuser= new Contact({
+            name,
+            email,
+            phone,
+            message,
+            activity
+        })
+            await saveuser.save();
+            res.status(200).json({message:"Enquiry Sent Successfully"})
+    
+    } catch (error) {
+        res.send({status:400,success:false,message:error.message})
+    }   
+});
 activity.get('/getActivities',activityController.getActivities)
 activity.get('/getActivitiesGroup',activityController.getActivitiesGroup)
 
