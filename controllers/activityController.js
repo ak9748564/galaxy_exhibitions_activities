712ffff427241b126e5392ba4e87e8f48b1ffdc6
ak9748564 <122973008+ak9748564@users.noreply.github.com>
2023-11-12 08:17:26 +0530

    ch1211230817

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -100,9 +100,9 @@ const getActivities = async(req,res) => {
 
 const getActivitiesGroup = async(req,res) => {
     try {
-        const response = await Activity.find().where('Status').equals('Active');
+        const response = await Activity.find({},{'Activity Master: Activity Master Number':1}).where('Status').equals('Active');
         // console.log(response)
-        res.send({status:200,message:'activities got successfully',data:response})
+        res.send({status:200,message:'activities groups got successfully',data:response})
     } catch (error) {
         res.send({message:error.message})
     }
